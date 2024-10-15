84c42e6bb5ffc7d4a2ec0b6bf2ba1fa15973dbbc
Jess4lxy <alex.mtz.rv17@gmail.com>
2024-10-14 21:04:20 -0600

    Update user.js with role

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 class User {
-    constructor(id, username, email) {
+    constructor(id, username, email, role) {
       this.id = id;
       this.username = username;
       this.email = email;
+      this.role = role;
     }
   }
   
