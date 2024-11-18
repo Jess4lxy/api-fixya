df97059b77560ca0946193436d88dd16475bfa92
Jess4lxy <alex.mtz.rv17@gmail.com>
2024-11-18 16:57:50 -0600

    cambios de datos en la validacion

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,10 +1,10 @@
 import { check, validationResult } from "express-validator";
 
 export const validateResident = [
-    check('nombre').notEmpty().withMessage('El nombre es requerido'),
-    check('idDepartamento').isInt().withMessage('El ID de departamento debe ser un número entero'),
-    check('numRegistro').notEmpty().withMessage('El número de registro es requerido'),
-    check('identificacion').notEmpty().withMessage('La identificación es requerida'),
+    check('name').notEmpty().withMessage('El nombre es requerido'),
+    check('idApartment').isInt().withMessage('El ID de departamento debe ser un número entero'),
+    check('numRegister').notEmpty().withMessage('El número de registro es requerido'),
+    check('identification').notEmpty().withMessage('La identificación es requerida'),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
@@ -13,7 +13,7 @@ export const validateResident = [
 ];
 
 export const validateDepartmentId = [
-    check("departmentId").isInt().withMessage("El ID de departamento debe ser un número entero"),
+    check("idApartment").isInt().withMessage("El ID de departamento debe ser un número entero"),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
