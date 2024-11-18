78669f3776de996b4d0c2b5d9ce225f550be2c9f
Jess4lxy <alex.mtz.rv17@gmail.com>
2024-11-18 12:13:47 -0600

    cambios en service model

--- models/service.js
+++ models/service.js
@@ -1,10 +1,10 @@
 export default class Service {
-    constructor(id, category, serviceType, basePrice, quantityAdjustment, description){
+    constructor(id, category, serviceType, description, basePrice, quantityAdjustment){
         this.id = id;
         this.category = category;
         this.serviceType = serviceType;
+        this.description = description;
         this.basePrice = basePrice;
         this.quantityAdjustment = quantityAdjustment;
-        this.description = description;
     }
 }
